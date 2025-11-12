819aec7b2af5450eb527ff2e0336745a5edcbe24
Noah Schroeder <noah.schroeder@gmail.com>
2025-11-12 16:11:05 -0700

    Test: Add test code for Hand

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -1,6 +1,8 @@
 public class BlackjackGame {
     public static void main(String[] args) {
-        // Example usage
+        // |TESTING::TESTING::TESTING|
+        // Create some cards and display their values
+        /*
         Card card1 = new Card(Suit.HEARTS, Rank.ACE);
         Card card2 = new Card(Suit.SPADES, Rank.KING);
 
@@ -9,5 +11,35 @@ public class BlackjackGame {
 
         System.out.println(card2); // Output: KING of SPADES
         System.out.println("Value: " + card2.getValue()); // Output: Value: 10
+        */
+
+        // Create and shuffle a deck, then deal a card
+        /*
+        Deck deck = new Deck();
+        deck.shuffle();
+        Card dealtCard = deck.dealCard();
+        System.out.println("Dealt Card: " + dealtCard);
+        System.out.println("Cards Left: " + deck.cardsLeft());
+        
+        // iteration 2
+        dealtCard = deck.dealCard();
+        System.out.println("Dealt Card: " + dealtCard);
+        System.out.println("Cards Left: " + deck.cardsLeft());
+        
+        // shuffle
+        deck.shuffle();
+
+        // iteration 3
+        dealtCard = deck.dealCard();
+        System.out.println("Dealt Card: " + dealtCard);
+        System.out.println("Cards Left: " + deck.cardsLeft());
+        */
+
+        /*// create a hand, add cards, and display hand
+        Hand hand = new Hand();
+        hand.addCard(new Card(Suit.HEARTS, Rank.ACE));
+        hand.addCard(new Card(Suit.SPADES, Rank.KING));
+        System.out.println(hand);
+        */
     }
 }
