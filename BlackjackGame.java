525d96094eaaa4c322c6563b70e05a0482f36150
Noah Schroeder <noah.schroeder@gmail.com>
2025-11-12 16:44:45 -0700

    Feat: Add player hitting rules

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -1,5 +1,75 @@
+import java.util.Scanner;
+
 public class BlackjackGame {
     public static void main(String[] args) {
+        // play again while loop
+        boolean playAgain = true;
+        Scanner scnr = new Scanner(System.in);
+        while (playAgain == true) {
+            // game logic
+            Deck deck = new Deck();
+            deck.shuffle();
+            Hand playerHand = new Hand();
+            Hand dealerHand = new Hand();
+            
+            // initial deal
+            playerHand.addCard(deck.dealCard());
+            Card upCard = deck.dealCard();
+            dealerHand.addCard(upCard);
+            playerHand.addCard(deck.dealCard());
+            dealerHand.addCard(deck.dealCard());
+
+            // show hands
+            System.out.println("Dealer: [" + upCard + "] Value: " + upCard.getValue());
+            System.out.println(playerHand);
+
+            // prompt player (hit or stand)
+            boolean playerTurn = true;
+            while (playerTurn) {
+                System.out.println("Would you like to (h)it or (s)tand?");
+                System.out.print(">>> ");
+                String playerChoice = scnr.nextLine();
+                if (playerChoice == "h") {
+                    //hit
+                    playerHand.addCard(deck.dealCard());
+                    System.out.println(playerHand);
+                    // check for bust
+                    if (playerHand.calculateValue() > 21) {
+                        System.out.println("Bust! You lose.");
+                        playerTurn = false;
+                    }
+                    else if (playerHand.calculateValue() == 21) {
+                        // end player turn
+                        playerTurn = false;
+                        System.out.println("21! You stand.");
+                    }
+                }
+
+                else if (playerChoice == "s") {
+                    //stand
+                    playerTurn = false;
+                }
+                else {
+                    System.out.println("Invalid choice, please enter 'h' or 's'.");
+                }
+
+
+
+
+
+            // playAgain false for testing
+            playAgain = false;
+        }
+    }
+       
+       
+       
+       
+       
+       
+       
+       
+       
         // |TESTING::TESTING::TESTING|
         // Create some cards and display their values
         /*
