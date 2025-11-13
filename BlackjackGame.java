75f39bd442db1130e6c191730cd27ca46c169674
Noah Schroeder <noah.schroeder@gmail.com>
2025-11-12 19:04:53 -0700

    Feat: Add player and dealer blackjack rules & add winners

--- BlackjackGame.java
+++ BlackjackGame.java
@@ -5,6 +5,7 @@ public class BlackjackGame {
         // play again while loop
         boolean playAgain = true;
         Scanner scnr = new Scanner(System.in);
+
         while (playAgain == true) {
             // game logic
             Deck deck = new Deck();
@@ -14,6 +15,7 @@ public class BlackjackGame {
             
             // initial deal
             playerHand.addCard(deck.dealCard());
+            // handle upcard
             Card upCard = deck.dealCard();
             dealerHand.addCard(upCard);
             playerHand.addCard(deck.dealCard());
@@ -23,20 +25,38 @@ public class BlackjackGame {
             System.out.println("Dealer: [" + upCard + "] Value: " + upCard.getValue());
             System.out.println(playerHand);
 
+            // check for player blackjack
+            if (playerHand.calculateValue() == 21) {
+                if (dealerHand.calculateValue() == 21) {
+                    System.out.println("Both you and the dealer have blackjack! It's a push.");
+                    System.exit(0);
+                }
+                else {
+                    System.out.println("Blackjack! You win!");
+                    System.exit(0);
+                }
+            }
+
+            // store sneaky dealer blackjack
+            boolean dealerBlackjack = false;
+            if (dealerHand.calculateValue() == 21) {
+                dealerBlackjack = true;
+            }
             // prompt player (hit or stand)
             boolean playerTurn = true;
-            while (playerTurn) {
+            while (playerTurn == true) {
                 System.out.println("Would you like to (h)it or (s)tand?");
                 System.out.print(">>> ");
                 String playerChoice = scnr.nextLine();
-                if (playerChoice == "h") {
+                if (playerChoice.equalsIgnoreCase("h")) {
                     //hit
                     playerHand.addCard(deck.dealCard());
+                    System.out.println("Dealer: [" + upCard + "] Value: " + upCard.getValue());
                     System.out.println(playerHand);
                     // check for bust
                     if (playerHand.calculateValue() > 21) {
                         System.out.println("Bust! You lose.");
-                        playerTurn = false;
+                        System.exit(0);
                     }
                     else if (playerHand.calculateValue() == 21) {
                         // end player turn
@@ -45,22 +65,63 @@ public class BlackjackGame {
                     }
                 }
 
-                else if (playerChoice == "s") {
+                else if (playerChoice.equalsIgnoreCase("s")) {
                     //stand
                     playerTurn = false;
                 }
                 else {
                     System.out.println("Invalid choice, please enter 'h' or 's'.");
                 }
+            }
 
+            // dealer turn
+            if (playerTurn == false) {
+                System.out.println("Dealer's turn...");
+                System.out.println(dealerHand);
 
+                // check for dealer blackjack
+                if (dealerBlackjack == true) {
+                    System.out.println("Dealer has blackjack! You lose.");
+                    System.exit(0);
+                }
 
+                // dealer hit until 17+
+                while (dealerHand.calculateValue() < 17) {
+                    dealerHand.addCard(deck.dealCard());
+                    System.out.println("Dealer hits...");
+                    System.out.println(dealerHand);
+                    }
+                }
+                // determine winner
+                int playerTotal = playerHand.calculateValue();
+                int dealerTotal = dealerHand.calculateValue();
+                if (dealerTotal > 21) {
+                    System.out.println("Dealer busts! You win!");
+                }
+                else if (dealerTotal > playerTotal) {
+                    System.out.println("Dealer wins with " + dealerTotal + " against your " + playerTotal + ".");
+                }
+                else if (dealerTotal < playerTotal) {
+                    System.out.println("You win with " + playerTotal + " against dealer's " + dealerTotal + "!");
+                }
+                else {
+                    System.out.println("It's a push at " + playerTotal + "and" + dealerTotal + "!");
+                }
+            }
 
-
-            // playAgain false for testing
-            playAgain = false;
+            // prompt to play again
+            System.out.println("Would you like to play again? (y/n)");
+            System.out.print(">>> ");
+            String againChoice = scnr.nextLine();
+            if (againChoice.equalsIgnoreCase("y")) {
+                playAgain = true;
+            }
+            else {
+                playAgain = false;
+            }
         }
-    }
+
+
        
        
        
@@ -112,4 +173,3 @@ public class BlackjackGame {
         System.out.println(hand);
         */
     }
-}
